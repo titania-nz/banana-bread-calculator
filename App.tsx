3fb476759dec9cde3aa13b576752054b1ae15581
Titania <kamiros.pallas@gmail.com>
2025-07-20 01:27:55 +1200

    Force preview update with simple background change

--- App.tsx
+++ App.tsx
@@ -293,7 +293,7 @@ export default function App() {
 
   return (
     <TooltipProvider delayDuration={300}>
-      <div className="min-h-screen bg-red-500">
+      <div className="min-h-screen" style={{backgroundColor: 'red'}}>
         {/* Screen Reader Announcements */}
         <div 
           className="sr-only" 
