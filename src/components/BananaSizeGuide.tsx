bc1e8f9cdecb43fff96be310b3312c421933cbea
Titania <kamiros.pallas@gmail.com>
2025-07-24 13:22:18 +1200

    Fix modal z-index to appear above footer

--- src/components/BananaSizeGuide.tsx
+++ src/components/BananaSizeGuide.tsx
@@ -23,7 +23,7 @@ export const BananaSizeGuide: React.FC<BananaSizeGuideProps> = ({ isOpen, onClos
 
   return (
     <div
-      className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4"
+      className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-[9999] p-4"
       onClick={handleOverlayClick}
       onKeyDown={handleKeyDown}
       role="dialog"
